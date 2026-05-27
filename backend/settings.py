266db434dae6340ac03f5c1df725cc9dbf790f3c
zeraus00 <193580639+zeraus00@users.noreply.github.com>
2026-05-27 21:25:35 +0800

    fix: fix the error in folder destination

--- backend/settings.py
+++ backend/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = ["*"]
 
 REST_FRAMEWORK  = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication..JWTAuthentication", 
+        "rest_framework_simplejwt.authentication.JWTAuthentication", 
     ),
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
@@ -56,6 +56,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api', # local apps
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -66,9 +69,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
-ROOT_URLCONF = 'simple_django_project.urls'
+ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
     {
@@ -134,3 +138,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWS_CREDENTIALS = True
